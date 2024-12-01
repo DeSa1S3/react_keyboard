c481defb6b80a17cf84a32d378ac95f7ed533bd1
StudyDevW <122658763+StudyDevW@users.noreply.github.com>
2024-12-01 20:10:24 +0400

    Реализовал нажатие на клавиши, отображение в input

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.sass'
+import '../preprocessor/index.sass'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <App/>
 )
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
